5ce13beda065a9850420f01d93098552f62bbd40
Oleh Zhaivoronok <zholeg@ukr.net>
2018-07-11 17:58:53 +0300

    add internationalisation for example

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+export const RU_TRANS = require(`raw-loader!./messages.ru.xlf`);
+
+const a = `
+
+`;
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [
+    {provide: TRANSLATIONS, useValue: RU_TRANS},
+    {provide: TRANSLATIONS_FORMAT, useValue: 'xlf'},
+    {provide: LOCALE_ID, useValue: 'ru'}
+  ]
+})
+.catch(err => console.log(err));
