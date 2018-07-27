a6eed51130016c8af32dd3c751ed142121f3a27f
Oleh Zhaivoronok <zholeg@ukr.net>
2018-07-27 12:58:57 +0300

    add 5 animation styles

--- src/app/animations/animation.ts
+++ src/app/animations/animation.ts
@@ -2,75 +2,169 @@ import { animate, state, style, transition, trigger, keyframes } from '@angular/
 
 export const HIDE_UP_DOWN_ANIMATION =
   trigger('hideUpDownTrigger', [
-    transition('hideDown => prev', animate(
-      '.6s 00ms ease-out',
-      keyframes([
-        style({ opacity: 0, transform: 'scale(0,0)' }),
-        style({ opacity: 0.33, transform: 'scale(1.1,1.1)' }),
-        style({ opacity: 0.66, transform: 'scale(0.95,0.95)' }),
-        style({ opacity: 1, transform: 'scale(1,1)' })
-      ])
-    )),
-    transition('hideUp => next', animate(
-      '.6s 00ms ease-out',
-      keyframes([
-        style({ opacity: 0, transform: 'scale(0,0)' }),
-        style({ opacity: 0.33, transform: 'scale(1.1,1.1)' }),
-        style({ opacity: 0.66, transform: 'scale(0.95,0.95)' }),
-        style({ opacity: 1, transform: 'scale(1,1)' })
-      ])
-    )),
-    transition('* => hideDown', animate(
+    transition('* => down_1', animate(
       '.3s 00ms ease-out',
       keyframes([
-        style({ opacity: 0, transform: 'scale(1,1)' }),
-        style({ opacity: 1, transform: 'scale(0,0)' })
+        style({ opacity: 1, transform: 'rotate(0deg) scale(1,1)' }),
+        style({ opacity: 0, transform: 'rotate(360deg) scale(0,0)' })
       ])
     )),
-    transition('* => hideUp', animate(
+    transition('* => up_1', animate(
       '.3s 00ms ease-out',
       keyframes([
-        style({ opacity: 0, transform: 'scale(1,1)' }),
-        style({ opacity: 1, transform: 'scale(0,0)' })
+        style({ opacity: 1, transform: 'scale(1,1) rotate(0deg)' }),
+        style({ opacity: 0, transform: 'scale(0,0) rotate(360deg)' })
+      ])
+    )),
+    transition('* => down_2', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1, transform: 'translateY(0%)' }),
+        style({ opacity: 0, transform: 'translateY(50%)' }),
+      ])
+    )),
+    transition('* => up_2', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1, transform: 'translateY(-0%)' }),
+        style({ opacity: 0, transform: 'translateY(-50%)' }),
+      ])
+    )),
+    transition('* => down_3', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1, transform: 'scale(1,1)' }),
+        style({ opacity: 0, transform: 'scale(0,0)' })
+      ])
+    )),
+    transition('* => up_3', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1, transform: 'scale(1,1)' }),
+        style({ opacity: 0, transform: 'scale(0,0)' })
+      ])
+    )),
+    transition('* => down_4', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1, transform: 'rotate(0deg)' }),
+        style({ opacity: 0, transform: 'rotate(360deg)' })
+      ])
+    )),
+    transition('* => up_4', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1, transform: 'rotate(0deg)' }),
+        style({ opacity: 0, transform: 'rotate(360deg)' })
+      ])
+    )),
+    transition('* => down_5', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1 }),
+        style({ opacity: 0 })
+      ])
+    )),
+    transition('* => up_5', animate(
+      '.3s 00ms ease-out',
+      keyframes([
+        style({ opacity: 1 }),
+        style({ opacity: 0 })
       ])
     ))
   ]);
 
-// export const HIDE_UP_DOWN_ANIMATION =
-//   trigger('hideUpDownTrigger', [
-//     transition('hideDown => prev', animate(
-//       '.6s 00ms ease-out',
-//       keyframes([
-//         style({ opacity: 0, transform: 'translateY(-75%)' }),
-//         style({ opacity: 0.33, transform: 'translateY(45px)' }),
-//         style({ opacity: 0.66, transform: 'translateY(-25px)' }),
-//         style({ opacity: 1, transform: 'translateY(0)' })
-//       ])
-//     )),
-//     transition('hideUp => next', animate(
-//       '.6s 00ms ease-out',
-//       keyframes([
-//         style({ opacity: 0, transform: 'translateY(75%)' }),
-//         style({ opacity: 0.33, transform: 'translateY(-45px)' }),
-//         style({ opacity: 0.66, transform: 'translateY(25px)' }),
-//         style({ opacity: 1, transform: 'translateY(0)' }),
-//       ])
-//     )),
-//     transition('* => hideDown', animate(
-//       '.3s 00ms ease-out',
-//       keyframes([
-//         style({ opacity: 1, transform: 'translateY(0%)' }),
-//         style({ opacity: 0, transform: 'translateY(50%)' }),
-//       ])
-//     )),
-//     transition('* => hideUp', animate(
-//       '.3s 00ms ease-out',
-//       keyframes([
-//         style({ opacity: 1, transform: 'translateY(-0%)' }),
-//         style({ opacity: 0, transform: 'translateY(-50%)' }),
-//       ])
-//     ))
-//   ]);
+export const SHOW_UP_DOWN_ANIMATION =
+  trigger('showUpDownTrigger', [
+    transition('* => up_1', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'scale(0,0) rotate(-00deg)' }),
+        style({ opacity: 0.33, transform: 'scale(1.15,1.15) rotate(270deg)' }),
+        style({ opacity: 0.66, transform: 'scale(0.90,0.90) rotate(360deg)' }),
+        style({ opacity: 1, transform: 'scale(1,1)' })
+      ])
+    )),
+    transition('* => down_1', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'scale(0,0) rotate(-00deg)' }),
+        style({ opacity: 0.33, transform: 'scale(1.15,1.15) rotate(270deg)' }),
+        style({ opacity: 0.66, transform: 'scale(0.90,0.90) rotate(360deg)' }),
+        style({ opacity: 1, transform: 'scale(1,1)' })
+      ])
+    )),
+    transition('* => up_2', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'translateY(-75%)' }),
+        style({ opacity: 0.33, transform: 'translateY(45px)' }),
+        style({ opacity: 0.66, transform: 'translateY(-25px)' }),
+        style({ opacity: 1, transform: 'translateY(0)' })
+      ])
+    )),
+    transition('* => down_2', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'translateY(75%)' }),
+        style({ opacity: 0.33, transform: 'translateY(-45px)' }),
+        style({ opacity: 0.66, transform: 'translateY(25px)' }),
+        style({ opacity: 1, transform: 'translateY(0)' }),
+      ])
+    )),
+    transition('* => up_3', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'scale(0,0)' }),
+        style({ opacity: 0.33, transform: 'scale(1.15,1.15)' }),
+        style({ opacity: 0.66, transform: 'scale(0.90,0.90)' }),
+        style({ opacity: 1, transform: 'scale(1,1)' })
+      ])
+    )),
+    transition('* => down_3', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'scale(0,0)' }),
+        style({ opacity: 0.33, transform: 'scale(1.15,1.15)' }),
+        style({ opacity: 0.66, transform: 'scale(0.90,0.90)' }),
+        style({ opacity: 1, transform: 'scale(1,1)' })
+      ])
+    )),
+    transition('* => up_4', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'rotate(0deg)' }),
+        style({ opacity: 0.33, transform: 'rotate(270deg)' }),
+        style({ opacity: 0.66, transform: 'rotate(390deg)' }),
+        style({ opacity: 1, transform: 'rotate(355deg)' }),
+        style({ opacity: 1, transform: 'rotate(360deg)' })
+      ])
+    )),
+    transition('* => down_4', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'rotate(0deg)' }),
+        style({ opacity: 0.33, transform: 'rotate(270deg)' }),
+        style({ opacity: 0.66, transform: 'rotate(390deg)' }),
+        style({ opacity: 1, transform: 'rotate(355deg)' }),
+        style({ opacity: 1, transform: 'rotate(360deg)' })
+      ])
+    )),
+    transition('* => up_5', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0 }),
+        style({ opacity: 1 })
+      ])
+    )),
+    transition('* => down_5', animate(
+      '.6s 00ms ease-out',
+      keyframes([
+        style({ opacity: 0 }),
+        style({ opacity: 1 })
+      ])
+    ))
+  ]);
 
 export const NO_YES_ANIMATION =
   trigger('noTrigger', [
@@ -83,5 +177,4 @@ export const NO_YES_ANIMATION =
         style({ opacity: 1, transform: 'translateX(-20px)' }),
       ])
     ))
-  ]
-  );
+  ]);
--- src/app/forms/study-words-from-sentence/study-words-from-sentence.component.ts
+++ src/app/forms/study-words-from-sentence/study-words-from-sentence.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { EmitData } from '../../services/global-data.service';
 import { ISentence } from '../../services/interfaces';
-import { HIDE_UP_DOWN_ANIMATION } from '../../animations/animation';
+import { HIDE_UP_DOWN_ANIMATION, SHOW_UP_DOWN_ANIMATION } from '../../animations/animation';
 @Component({
   selector: 'app-study-words-from-sentence',
   templateUrl: './study-words-from-sentence.component.html',
   styleUrls: ['./study-words-from-sentence.component.scss'],
-  animations: [HIDE_UP_DOWN_ANIMATION]
+  animations: [HIDE_UP_DOWN_ANIMATION, SHOW_UP_DOWN_ANIMATION]
 })
 
 export class StudyWordsFromSentenceComponent implements OnInit, AfterViewInit {
@@ -15,7 +15,11 @@ export class StudyWordsFromSentenceComponent implements OnInit, AfterViewInit {
 
   private sentences: Array<ISentence> = [];
   private currentSentence: number;
-  private upDown = '';
+  private triggerAnimation = {
+    hide: '',
+    show: ''
+  };
+  private numberTransition: number;
 
   constructor(private emitData: EmitData) {
 
@@ -47,19 +51,28 @@ export class StudyWordsFromSentenceComponent implements OnInit, AfterViewInit {
     this.emitData.sentence.emit(this.sentences[this.currentSentence]);
   }
 
+  getRandomArbitary(min: number, max: number): number {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
   changeSentence(newNum: number, upDown: string) {
-    if (newNum >= 0 && newNum < (this.sentences.length)) {
+    if (
+      newNum >= 0 &&
+      newNum < (this.sentences.length)
+    ) {
       this.currentSentence = newNum;
-      this.upDown = upDown;
+
+      this.numberTransition = this.getRandomArbitary(1, 5);
+      this.triggerAnimation.hide = '' + upDown + '_' + this.numberTransition;
     }
   }
 
   previousSentence() {
-    this.changeSentence(this.currentSentence - 1, 'hideUp');
+    this.changeSentence(this.currentSentence - 1, 'up');
   }
 
   nextSentence() {
-    this.changeSentence(this.currentSentence + 1, 'hideDown');
+    this.changeSentence(this.currentSentence + 1, 'down');
   }
 
   swipe(event) {
@@ -71,10 +84,10 @@ export class StudyWordsFromSentenceComponent implements OnInit, AfterViewInit {
   }
 
   animEnd(event) {
-    if (this.upDown === 'hideUp') {
-      this.upDown = 'next';
-    } else {
-      this.upDown = 'prev';
+    if (this.triggerAnimation.hide === 'up' + '_' + this.numberTransition) {
+      this.triggerAnimation.show = 'down' + '_' + this.numberTransition;
+    } else if (this.triggerAnimation.hide === 'down' + '_' + this.numberTransition) {
+      this.triggerAnimation.show = 'up' + '_' + this.numberTransition;
     }
     this.emitData.sentence.emit(this.sentences[this.currentSentence]);
   }
