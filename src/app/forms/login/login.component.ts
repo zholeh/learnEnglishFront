1318e91c3e8a28bd7e6ec8da240d99d1baa9bd9d
Oleh Zhaivoronok <zholeg@ukr.net>
2018-07-27 18:36:53 +0300

    write suer language to local setting

--- src/app/forms/login/login.component.ts
+++ src/app/forms/login/login.component.ts
@@ -20,6 +20,7 @@ export class LoginComponent implements OnInit {
   }
 
   private changeLanguage(lang: string) {
+    this.data.setLocalStorage('userLanguage', lang);
     this.data.changeLanguage(lang);
   }
 
